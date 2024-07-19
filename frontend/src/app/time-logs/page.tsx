214d8d265fa67a101a6ead1ff7537fac67d2d3b6
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-07-19 23:48:09 +0545

    feat: add time logs table

--- frontend/src/app/time-logs/page.tsx
+++ frontend/src/app/time-logs/page.tsx
@@ -1,12 +1,63 @@
 import MainLayout from "@/components/main-layout";
-import { Button } from "@/components/ui/button";
-
 import type { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const metadata: Metadata = {
   title: "Time Logs - Sandbox HRMS",
   description: "Human Resource Management System",
 };
+
+const data = [
+  {
+    id: "1",
+    start: "Jul 19 23:46",
+    end: "Jul 19 23:46",
+    duration: "10h55m",
+    project: "Project L",
+    activity: "System Administration",
+  },
+  {
+    id: "1",
+    start: "Jul 19 23:46",
+    end: "Jul 19 23:46",
+    duration: "10h55m",
+    project: "Project L",
+    activity: "System Administration",
+  },
+  {
+    id: "1",
+    start: "Jul 19 23:46",
+    end: "Jul 19 23:46",
+    duration: "10h55m",
+    project: "Project L",
+    activity: "System Administration",
+  },
+
+  {
+    id: "1",
+    start: "Jul 19 23:46",
+    end: "Jul 19 23:46",
+    duration: "10h55m",
+    project: "Project L",
+    activity: "System Administration",
+  },
+  {
+    id: "1",
+    start: "Jul 19 23:46",
+    end: "Jul 19 23:46",
+    duration: "10h55m",
+    project: "Project L",
+    activity: "System Administration",
+  },
+];
 
 export default function TimeLogs() {
   return (
@@ -14,20 +65,31 @@ export default function TimeLogs() {
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Time Logs</h1>
       </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-        x-chunk="dashboard-02-chunk-1"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            Page under construction
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            You can start by adding time logs.
-          </p>
-          <Button className="mt-4">Go to Time Logs</Button>
-        </div>
-      </div>
+      <Table>
+        <TableCaption>Showing 7 out of 300.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Start</TableHead>
+            <TableHead>End</TableHead>
+            <TableHead>Duration</TableHead>
+            <TableHead>Project</TableHead>
+            <TableHead>Activity</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((invoice) => (
+            <TableRow key={invoice.id}>
+              <TableCell className="font-medium">{invoice.id}</TableCell>
+              <TableCell>{invoice.start}</TableCell>
+              <TableCell>{invoice.end}</TableCell>
+              <TableCell>{invoice.duration}</TableCell>
+              <TableCell>{invoice.project}</TableCell>
+              <TableCell>{invoice.activity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </MainLayout>
   );
 }
