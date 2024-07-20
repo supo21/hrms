ffa3c30254783e6204bcac664c295f87166f59bf
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-07-20 13:30:38 +0545

    feat: add user admin and fix user model issues

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,25 @@
+from core.models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        *BaseUserAdmin.fieldsets,
+        (
+            "Expected working hours",
+            {
+                "fields": (
+                    "expected_hours_sun",
+                    "expected_hours_mon",
+                    "expected_hours_tue",
+                    "expected_hours_wed",
+                    "expected_hours_thu",
+                    "expected_hours_fri",
+                    "expected_hours_sat",
+                )
+            },
+        ),
+    )
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import Group
-from django.contrib.auth.models import Permission
 from django.db import models
 
 
@@ -36,21 +34,6 @@ class User(AbstractUser):
     )
     expected_hours_sat = models.IntegerField(
         default=0, null=False, blank=False
-    )
-
-    groups = models.ManyToManyField(
-        Group,
-        related_name="core_user_groups_set",
-        blank=True,
-        help_text="The groups this user belongs to.",
-        related_query_name="core_user",
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="core_user_permissions_set",
-        blank=True,
-        help_text="Specific permissions for this user.",
-        related_query_name="core_user_permissions",
     )
 
     def __str__(self) -> str:
