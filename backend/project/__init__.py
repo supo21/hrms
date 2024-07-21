7622ba4c5a9165f93052761f88ba3652bf02385f
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-07-21 16:34:55 +0545

    chore: fix backend build

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -1,5 +1,3 @@
 import django_stubs_ext
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.viewsets import ModelViewSet
 
-django_stubs_ext.monkeypatch(extra_classes=(ModelViewSet, GenericViewSet))
+django_stubs_ext.monkeypatch()
