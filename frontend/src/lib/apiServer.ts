e0beed537cc62e6450d628e94cf8c6b39d1023e2
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-07-23 16:20:22 +0545

    refactor: improve apiServer code

--- frontend/src/lib/apiServer.ts
+++ frontend/src/lib/apiServer.ts
@@ -3,134 +3,114 @@ import { redirect } from "next/navigation";
 import { API_HOST } from "./constants";
 import { components } from "./schema";
 
-export async function getCurrentTimeLog() {
+function isRedirectError(err: any) {
+  return (
+    err &&
+    typeof err === "object" &&
+    "digest" in err &&
+    typeof err.digest === "string" &&
+    err.digest.startsWith("NEXT_REDIRECT")
+  );
+}
+
+export async function serverFetch(url: string, init?: RequestInit) {
   const cookieStore = cookies();
   const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+  if (sessionid) {
+    if (!init) init = {};
+    init.headers = { ...init.headers, Cookie: `sessionid=${sessionid.value}` };
+  }
+  const res = await fetch(`${API_HOST}${url}`, init);
+  if (res.status === 401) redirect("/login/");
+  return res;
+}
+
+export async function getCurrentTimeLog(): Promise<
+  components["schemas"]["TimeLogDTO"] | null
+> {
   try {
-    const res = await fetch(`${API_HOST}/api/time-logs/current/`, {
-      headers: {
-        Cookie: `sessionid=${sessionid.value}`,
-      },
-    });
-    if (res.status === 401) redirect("/login/");
+    const res = await serverFetch("/api/time-logs/current/");
     if (!res.ok) return null;
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
     return null;
   }
 }
 
-export async function getProjects() {
-  const cookieStore = cookies();
-  const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+export async function getProjects(): Promise<
+  components["schemas"]["PagedProjectDTO"]
+> {
   try {
-    const res = await fetch(`${API_HOST}/api/projects/`, {
-      method: "GET",
-      headers: {
-        Cookie: `sessionid=${sessionid.value}`,
-      },
-    });
-    if (!res.ok) {
-      return null;
-    }
+    const res = await serverFetch("/api/time-logs/current/");
+    if (!res.ok) return { count: 0, items: [] };
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
-    return null;
+    return { count: 0, items: [] };
   }
 }
 
-export async function getActivities() {
-  const cookieStore = cookies();
-  const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+export async function getActivities(): Promise<
+  components["schemas"]["PagedActivityDTO"]
+> {
   try {
-    const res = await fetch(`${API_HOST}/api/activities/`, {
-      method: "GET",
-      headers: {
-        Cookie: `sessionid=${sessionid.value}`,
-      },
-    });
-    if (!res.ok) {
-      return null;
-    }
+    const res = await serverFetch("/api/activities/");
+    if (!res.ok) return { count: 0, items: [] };
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
-    return null;
+    return { count: 0, items: [] };
   }
 }
 
-export async function getCurrentUser() {
-  const cookieStore = cookies();
-  const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+export async function getCurrentUser(): Promise<
+  components["schemas"]["UserDTO"]
+> {
   try {
-    const res = await fetch(`${API_HOST}/api/users/current/`, {
-      method: "GET",
-      headers: {
-        Cookie: `sessionid=${sessionid.value}`,
-      },
-    });
-    if (!res.ok) {
-      return null;
-    }
+    const res = await serverFetch("/api/users/current/");
+    if (!res.ok) redirect("/login/");
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
-    return null;
+    redirect("/login/");
   }
 }
 
 export async function getHolidays(
   page: number = 1,
   limit: number = 10
-): Promise<components["schemas"]["PagedHolidayDTO"] | null> {
-  const cookieStore = cookies();
-  const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+): Promise<components["schemas"]["PagedHolidayDTO"]> {
   try {
-    const res = await fetch(
-      `${API_HOST}/api/holidays/?limit=${limit}&offset=${(page - 1) * limit}`,
-      {
-        headers: {
-          Cookie: `sessionid=${sessionid.value}`,
-        },
-      }
+    const res = await serverFetch(
+      `/api/holidays/?limit=${limit}&offset=${(page - 1) * limit}`
     );
-    if (res.status === 401) redirect("/login/");
-    if (!res.ok) return null;
+    if (!res.ok) return { count: 0, items: [] };
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
-    return null;
+    return { count: 0, items: [] };
   }
 }
 
 export async function getTimeLogs(
   page: number = 1,
   limit: number = 10
-): Promise<components["schemas"]["PagedTimeLogDTO"] | null> {
-  const cookieStore = cookies();
-  const sessionid = cookieStore.get("sessionid");
-  if (!sessionid) redirect("/login/");
+): Promise<components["schemas"]["PagedTimeLogDTO"]> {
   try {
-    const res = await fetch(
-      `${API_HOST}/api/time-logs/?limit=${limit}&offset=${(page - 1) * limit}`,
-      {
-        headers: {
-          Cookie: `sessionid=${sessionid.value}`,
-        },
-      }
+    const res = await serverFetch(
+      `/api/time-logs/?limit=${limit}&offset=${(page - 1) * limit}`
     );
-    if (res.status === 401) redirect("/login/");
-    if (!res.ok) return null;
+    if (!res.ok) return { count: 0, items: [] };
     return await res.json();
   } catch (err) {
+    if (isRedirectError(err)) throw err;
     console.log(err);
-    return null;
+    return { count: 0, items: [] };
   }
 }
